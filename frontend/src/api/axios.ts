deedeed802490a2250cd6e302f5275b631e040e3
PriyanjalDurgapal <durgapalpriyanjal@gmail.com>
2026-03-25 14:15:37 +0530

    frontend

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,9 +1,14 @@
 import axios from "axios";
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api"
-});
+const BASE_URL = import.meta.env.VITE_API_URL;
 
+if (!BASE_URL) {
+  console.error("VITE_API_URL is missing!");
+}
+
+const api = axios.create({
+  baseURL: `${BASE_URL}/api`,
+});
 // Attach JWT automatically
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
